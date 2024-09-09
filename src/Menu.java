7f4ae50cc47775da1fb59e9571e2230d586fe6f1
SomeKristi <kmaas2007@gmail.com>
2024-09-09 09:40:52 +0200

    commit 3

--- src/Menu.java
+++ src/Menu.java
@@ -5,21 +5,25 @@ import java.util.concurrent.ExecutionException;
 public class Menu {
     private static Scanner scanner = new Scanner(System.in);
     private static ArrayList<Mazlicek> mazlic;
+    private static boolean running = true;
 
     public static void start(ArrayList<Mazlicek> mazlicci) {
         mazlic = mazlicci;
-        boolean running = true;
         String input;
         System.out.println("Running mazlíčci manager");
         while (running) {
             try {
                 System.out.print("action: ");
                 input = scanner.nextLine();
+                input = input.toLowerCase();
                 switch (input) {
                     case "help" -> help();
                     case "view" -> view();
+                    case "view all" -> viewall();
+                    case "view by name" -> viewByName();
                     case "add" -> add();
                     case "remove" -> remove();
+                    case "exit" -> exit();
                     default -> help();
                 }
             } catch (Exception e) {
@@ -32,12 +36,33 @@ public class Menu {
     private static void help() {
         System.out.println("You can use: help, view, add, remove");
     }
+
     private static void view() {
         System.out.print("index: ");
         int param = scanner.nextInt();
         scanner.nextLine();
         System.out.println(mazlic.get(param));
     }
+
+    private static void viewByName() {
+        String input = scanner.nextLine();
+        Mazlicek mazlicek = mazlic.stream().filter(mazlicekus -> input.equals(mazlicekus.getJmeno())).findFirst().orElse(null);
+        if (mazlicek == null)
+            throw new RuntimeException("Mazlicek with name " + input + " does not exist");
+        System.out.println(mazlicek);
+    }
+
+    private static void viewall() {
+        System.out.println("Jmena mazlicku jsou:");
+        mazlic.forEach(mazlicek -> {
+            System.out.print(mazlicek.getJmeno());
+            if (!mazlicek.equals(mazlic.getLast()))
+                System.out.println(", ");
+            else
+                System.out.println(".");
+        });
+    }
+
     private static void add() {
         System.out.println("format: jmeno druh vek vaha");
         System.out.print("adding: ");
@@ -55,20 +80,26 @@ public class Menu {
                             )
                     )
             );
-            System.out.println(params[0]+" was added");
+            System.out.println(params[0] + " was added");
         } catch (Exception e) {
             System.out.printf(e.toString());
         }
     }
+
     private static void remove() {
         System.out.print("index: ");
         int param = scanner.nextInt();
         scanner.nextLine();
         try {
             mazlic.remove(param);
-            System.out.println(mazlic.get(param).getJmeno()+" was removed");
+            System.out.println(mazlic.get(param).getJmeno() + " was removed");
         } catch (Exception e) {
             System.out.printf(e.toString());
         }
+    }
+
+    private static void exit() {
+        System.out.println("Bye bye");
+        running = false;
     }
 }
